e101a487ee3f8bbeb6510b8bd4508b7be72bad2d
Himu1503 <54092681+Himu1503@users.noreply.github.com>
2022-03-28 11:57:48 +0100

    Django setup done

---
+++ ecom/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import URLPattern, path,include 
+from rest_framework.authtoken import views 
+from .views import home 
+
+urlpatterns =[
+    path('', home, name='api.home'),
+    path('category/', include('api.category.urls'))
+]
