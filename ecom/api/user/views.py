111facdbf80ab41b156b538d2344cc07a837963c
Himu1503 <54092681+Himu1503@users.noreply.github.com>
2022-03-30 13:19:02 +0100

    Order API

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -87,11 +87,13 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset= CustomUser.objects.all().order_by()
     serializer_class= UserSerializer
 
-    def get_permissions(self):
-        try: 
-            return [permissions() for permission in self.permission_classes_by_action[self.action]]
 
-        except KeyError:
-            return [permissions() for permission in self.permission_classes]
+    def get_permissions(self):
+        try:
+            # return permission_classes depending on `action` 
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError: 
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
 
 
