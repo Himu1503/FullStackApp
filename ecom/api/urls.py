2512f01c85859f0bf3a5ddfa27bebb0206d4695d
Himu1503 <54092681+Himu1503@users.noreply.github.com>
2022-04-01 14:08:42 +0100

    Payment API (Braintree)

--- ecom/api/urls.py
+++ ecom/api/urls.py
@@ -4,10 +4,12 @@ from .views import home
 
 
 urlpatterns =[
+    
     path('', home, name='api.home'),
     path('category/', include('api.category.urls')),
     path('product/', include('api.product.urls')),
     path('user/', include('api.user.urls')),
-    path('order/',include('api.order.urls'))
+    path('order/',include('api.order.urls')),
+    path('payment/',include('api.payment.urls')),
 
 ]
